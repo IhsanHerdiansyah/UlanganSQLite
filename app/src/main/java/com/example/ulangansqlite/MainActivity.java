ad5e9dc1ed5683b6759db976ec19040ae9c9fa59
adriansatria <adriansatria.siswa@smkn4bdg.sch.id>
2021-02-21 23:11:00 +0700

    Push pertama

---
+++ app/src/main/java/com/example/ulangansqlite/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.ulangansqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvSplash;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvSplash = findViewById(R.id.textsplash);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000L); // 4000 L = 4 DETIK
+    }
+}
